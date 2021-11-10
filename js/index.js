39c44c942868e9f50aa8fc7edefc454da3fffd45
wesleyfelix <wesley.felixdelima96@gmail.com>
2021-11-10 11:16:33 -0300

    ultima aula

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida nutricionista";
+
+var primeiro = document.querySelector("#primeiro-paciente");
+console.log(primeiro);
