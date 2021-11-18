cade62a0c15f1c7b9f12deb255e96d57630737dd
wesleyfelix <wesley.felixdelima96@gmail.com>
2021-11-18 09:41:30 -0300

    calculando imc com javascript

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida nutricionista";
 
-var primeiro = document.querySelector("#primeiro-paciente");
-console.log(primeiro);
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+console.log(paciente); // tr
+console.log(tdPeso); // td que tem o peso
+console.log(tdAltura); // td que tem a altura
+console.log(peso); // obter 100
+console.log(altura); // obter 2.00
+
+var tdImc = paciente.querySelector(".info-imc");
+
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
+
+console.log(imc);
