20be7100f9c821ba268f6471296929bc702b2402
wesleyfelix <wesley.felixdelima96@gmail.com>
2021-11-29 09:00:00 -0300

    ajustando funcoes js

--- js/form.js
+++ js/form.js
@@ -20,6 +20,7 @@ botaoAdicionar.addEventListener("click", function (event){
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso, altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
